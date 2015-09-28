e44f013a0d0fb1478af5f91af7e6748823e2fcbf
thez3ro <silvethebest@yahoo.it>
2015-09-28 18:22:55 +0200

    semi-standard syntax

--- app/app.js
+++ app/app.js
@@ -1,73 +1,63 @@
-var colors = require('colors');
+require('colors');
 
 var argv = require('yargs')
-    .example('$0 --www ./www --plugins ./app/plugins -p 13370', '->'.red+' Start penmode3 on port 13370 with folder')
-		.option('www', {
-	    describe: "Load web server from",
-	  })
-		.option('plugins', {
-			describe: "Load plugins from",
-		})
-		.option('no-serve', {
-			describe: "Don't start the web server",
-		})
-		.option('port', {
-			alias: 'p',
-			describe: "Socket.io [+ Server] Port",
-		})
+		.example('$0 --www ./www --plugins ./app/plugins -p 13370', '->'.red + ' Start penmode3 on port 13370 with folder')
+		.option('www', {describe: 'Load web server from'})
+		.option('plugins', {describe: 'Load plugins from'})
+		.option('no-serve', {describe: 'Don\'t start the web server'})
+		.option('port', {alias: 'p', describe: 'Socket.io [+ Server] Port'})
     .help('h')
     .alias('h', 'help')
 		.wrap(null)
-    .epilog('Do what you want cause a pirate is free! \n'+'You are a Pirate!'.rainbow)
+    .epilog('Do what you want cause a pirate is free! \n' + 'You are a Pirate!'.rainbow)
     .argv;
 
-if(!argv.noServe){
-	var express = require('express')
+if(!argv.noServe) {
+  var express = require('express');
 	var app = express();
-	var server = require('http').createServer(app);
-	var io = require('socket.io')(server);
-}else{
+  var server = require('http').createServer(app);
+  var io = require('socket.io')(server);
+} else {
 	var io = require('socket.io')();
 }
 
 var request = require('request');
-var proc = require('child_process');
-var plugins = [],
-	proc_n = null,
-	proc = null;
+var child_process = require('child_process');
+var plugins = [];
+var proc_n = null;
 
 var jwt = require('./jwt.js');
 var login = require('./login.json').login;
 var auth = [];
 
-var WEBPATH = require('path').resolve(__dirname,'../www');
-var PLUGINPATH = require('path').resolve(__dirname,'./plugins');
+var WEBPATH = require('path').resolve(__dirname, '../www');
+var PLUGINPATH = require('path').resolve(__dirname, './plugins');
 var PORT = 13370;
 var STATUS = {
-	start: 1,
-	end: 2,
-}
+  started: 1,
+  ended: 2
+};
 
 // Load Plugins
-require('fs').readdirSync(argv.plugins || PLUGINPATH).forEach(function(file) {
+require('fs').readdirSync(argv.plugins || PLUGINPATH).forEach(function (file) {
   if (file.match(/\.js$/) !== null && file !== 'main.js') {
     var name = file.replace('.js', '');
 		plugins.push(name);
 	}
-})
-console.log("Plugins loaded: "+plugins.join(", "));
+});
+console.log('Plugins loaded: ' + plugins.join(', '));
 
-function getTor(cb) {
+function getTor (cb) {
   request.get({
-    url: 'https://check.torproject.org/',
-  }, function(err, resp, body) {
+    url: 'https://check.torproject.org/'
+  }, function (err, resp, body) {
     if(!err) {
       if(resp.statusCode === 200) {
-				if(body.indexOf("tor-off.png") > -1) {
+				if(body.indexOf('tor-off.png') > -1) {
 					return cb(false);
-				}else if (body.indexOf("tor-on.png") > -1) {
+				} else if (body.indexOf('tor-on.png') > -1) {
 					return cb(true);
-				}else{
+				} else {
 					return cb(undefined);
 				}
       }
@@ -75,13 +65,13 @@ function getTor(cb) {
   });
 }
 
-function getIP(cb) {
+function getIP (cb) {
   request.get({
     url: 'http://ipinfo.io/ip',
-		json: true
-  }, function(err, resp, body) {
-    if(!err) {
-      if(resp.statusCode === 200) {
+    json: true
+  }, function (err, resp, body) {
+    if (!err) {
+      if (resp.statusCode === 200) {
         return cb(body);
       }
     }
@@ -89,26 +79,26 @@ function getIP(cb) {
 }
 
 // Socket.io connection
-io.on('connection', function(socket) {
-	var p_callback = function(error, stdout, stderr) {
-		socket.emit('console', ""+stdout);
-		socket.emit('console', ""+stderr);
+io.on('connection', function (socket) {
+	var p_callback = function (error, stdout, stderr) {
+		socket.emit('console', '' + stdout);
+		socket.emit('console', '' + stderr);
 		//io.sockets.emit('status', null);
-	}
+	};
 
-	var check_auth = function(socket_id){
-		if(auth.indexOf(socket_id) > -1){
+	var check_auth = function (socket_id) {
+		if(auth.indexOf(socket_id) > -1) {
 			return true;
 		}
 		return false;
-	}
+	};
 
 	socket.emit('login_required');
 
-	socket.on('login', function(hash){
-		for(var i=0;i<login.length;i++){
-			jwt.verify(hash, login[i].pass, function(err, decoded) {
-				if(decoded.user===login[i].user){
+	socket.on('login', function (hash) {
+		for(var i = 0; i < login.length; i++) {
+			jwt.verify(hash, login[i].pass, function (err, decoded) {
+				if (decoded.user === login[i].user) {
 					console.log(socket.id);
 					auth.push(socket.id);
 					socket.emit('authenticated');
@@ -117,17 +107,17 @@ io.on('connection', function(socket) {
 		}
 	});
 
-	socket.on('get_command_list', function(){
+	socket.on('get_command_list', function () {
 		if(!check_auth(socket.id)) { socket.emit('login_required'); return; }
 		socket.emit('command_list', plugins);
 	});
 
-	socket.on('ip_status', function(){
+	socket.on('get_ip_status', function () {
 		if(!check_auth(socket.id)) { socket.emit('login_required'); return; }
 		var ip_status = {};
-		getIP(function(e_ip){
-			ip_status.ip = e_ip.replace(/\n/g,'');
-			getTor(function(tor_status){
+		getIP(function (e_ip) {
+			ip_status.ip = e_ip.replace(/\n/g, '');
+			getTor(function (tor_status) {
 				ip_status.tor = tor_status;
 				socket.emit('ip_status', ip_status);
 			});
@@ -135,10 +125,10 @@ io.on('connection', function(socket) {
 	});
 
 	// When the client says to start a server...
-	socket.on('start_command', function(name) {
+	socket.on('start_command', function (name) {
 		if(!check_auth(socket.id)) { socket.emit('login_required'); return; }
 		// If a server is already running or server doesn't exist
-		if (proc || !plugins[name]) {
+		if (proc_n || !plugins[name]) {
 			// Let the user know that it failed.
 			socket.emit('fail', 'start_server');
 			// Stop execution of this callback
@@ -148,48 +138,39 @@ io.on('connection', function(socket) {
 		// Set which server is currently running
 		proc_n = name;
 
-		require('./plugins/'+plugins[name]+'.js')(p_callback,function(proc,type){
-      if(type=="spawn"){
+		require('./plugins/' + plugins[name] + '.js')(p_callback, function (proc, type) {
+      if (type === 'spawn') {
 				proc.stdout.on('data', function (data) {
 					if (data) {
-						socket.emit('console', ""+data);
+						socket.emit('console', '' + data);
 					}
 				});
 
 				proc.stderr.on('data', function (data) {
 					if (data) {
-						socket.emit('console', ""+data);
+						socket.emit('console', '' + data);
 					}
 				});
 
 				proc.on('exit', function () {
 					proc = proc_n = null;
-					socket.emit('status', STATUS.end);
+					socket.emit('status', STATUS.ended);
 				});
       }
     });
 
-		socket.emit('status', STATUS.start);
-
+		socket.emit('status', STATUS.started);
 	}); // End .on('start_server')
 
-	socket.on('command', function(cmd) {
+	socket.on('command', function (cmd) {
 		if(!check_auth(socket.id)) { socket.emit('login_required'); return; }
 		if (proc) {
-			io.sockets.emit('console', "Player Command: " + cmd);
+			sockets.emit('console', 'Player Command: ' + cmd);
 			proc.stdin.write(cmd + "\r");
 		} else {
 			socket.emit('fail', cmd);
 		}
 	});
-});
-
-// Allows me to type commands into the Console Window
-process.stdin.resume();
-process.stdin.on('data', function (data) {
-	if (proc) {
-		proc.stdin.write(data);
-	}
 });
 
 // ExpressStatic file server
--- www/js/main.js
+++ www/js/main.js
@@ -1,56 +1,56 @@
-function getHash(lhash){
-  if(lhash==""){
+function getHash (lhash) {
+  if(lhash === '') {
     $('#extra-content').hide();
     $('#main-content').show();
     $('#extra-content').html('');
-    $('a[href="#"]').click(function(){
-        $("li.active").removeClass("active");
-        $(this).parent().addClass("active");
+    $('a[href="#"]').click(function () {
+        $('li.active').removeClass('active');
+        $(this).parent().addClass('active');
     });
-  }else{
-    var hash = lhash.replace(/\#/g,'');
+  } else {
+    var hash = lhash.replace(/\#/g, '');
     $.get('template/'+hash+'.html', function(c) {
       $('#extra-content').html(c);
       $('#main-content').hide();
       $('#extra-content').show();
-      $('a[href="#'+hash+'"]').click(function(){
-          $("li.active").removeClass("active");
-          $(this).parent().addClass("active");
+      $('a[href="#' + hash + '"]').click(function () {
+          $('li.active').removeClass('active');
+          $(this).parent().addClass('active');
       });
     });
   }
 }
 
 if(typeof io !== 'undefined'){
-  var socket = io("http://localhost:13370");
-  socket.on('login_required', function(){
-    if(window.sessionStorage && sessionStorage.token!=undefined && sessionStorage.remember_me==1){
-      socket.emit('login', sessionStorage.token);
+  var socket = window.io('http://localhost:13370');
+  socket.on('login_required', function () {
+    if(window.sessionStorage && window.sessionStorage.token !== undefined && window.sessionStorage.remember_me === 1) {
+      socket.emit('login', window.sessionStorage.token);
       $('#logout').show();
-      $('#logout-button').click(function(){
-        sessionStorage.token=null;
-        sessionStorage.remember_me=0;
+      $('#logout-button').click(function () {
+        sessionStorage.token = null;
+        sessionStorage.remember_me = 0;
         window.history.go(0);
       });
-    }else{
+    } else {
       $.get('template/login.html', function(c) {
         $('#main-content').html(c);
-        $('#login').click(function(){
-          var token = WebJWT.sign({"user":$("#inputUser").val()},$("#inputPassword").val());
-          if(window.sessionStorage){
-            sessionStorage.token=token;
-            sessionStorage.remember_me=($('#remember-me').is(':checked')?1:0);
+        $('#login').click(function () {
+          var token = WebJWT.sign({'user':$('#inputUser').val()}, $('#inputPassword').val());
+          if(window.sessionStorage) {
+            sessionStorage.token = token;
+            sessionStorage.remember_me = ($('#remember-me').is(':checked') ? 1 : 0);
           }
           socket.emit('login', token);
         });
-        $('#remember-me').click(function() {
+        $('#remember-me').click(function () {
             // Was false, become true with this click
             if ($(this).is(':checked')) {
-              if(window.sessionStorage){
-                sessionStorage.remember_me=1;
+              if(window.sessionStorage) {
+                sessionStorage.remember_me = 1;
                 return true; // Remain true
-              }else{
-                alert("sessionStorage is not supported!");
+              } else {
+                alert('sessionStorage is not supported!');
                 return false; // Become false
               }
             }
@@ -58,38 +58,36 @@ if(typeof io !== 'undefined'){
       });
     }
   });
-  socket.on('authenticated', function(){
-    $.get('template/home.html', function(c) {
+  socket.on('authenticated', function () {
+    $.get('template/home.html', function (c) {
       $('#main-content').html(c);
     });
     socket.emit('get_command_list');
-    socket.emit('ip_status');
+    socket.emit('get_ip_status');
   });
-  socket.on('command_list',function(arr){
-    $.get('template/command_item.mst', function(template) {
-      var rendered = Mustache.render(template, {'commands':arr});
+  socket.on('command_list', function (arr) {
+    $.get('template/command_item.mst', function (template) {
+      var rendered = Mustache.render(template, {'commands': arr});
       $('.command-list').html(rendered);
       $('a.command').click(function() {
         alert($(this).data('command'));
       });
     });
   });
-  socket.on('ip_status',function(ip_status){
-    $("#ip_status").html("<b>Penmode IP:</b> "+ip_status.ip+", <b>Tor:</b> "+(ip_status.tor?"<span class='green'>Enabled":"<span class='red'>Disabled")+"</span>");
-    $(".footer").show();
+  socket.on('ip_status', function (ip_status) {
+    $('#ip_status').html(
+      '<b>Penmode IP:</b> ' + ip_status.ip +
+      ', <b>Tor:</b> ' + (ip_status.tor ? '<span class="green">Enabled' : '<span class="red">Disabled') +
+      '</span>');
+    $('.footer').show();
   });
   socket.on('disconnect', function(){});
 }
 
-$(window).on('hashchange', function() {
-  getHash(location.hash);
+$(window).on('hashchange', function () {
+  getHash(window.location.hash);
 });
 
-if(location.hash!=""){
-  getHash(location.hash);
+if (window.location.hash !== '') {
+  getHash(window.location.hash);
 }
-
-/*$.get('template/no-home.mst', function(template) {
-  var rendered = Mustache.render(template, {});
-  $('#content').html(rendered);
-});*/
