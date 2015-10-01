943fc09128738de88a215f7e4894bc24f330ef3e
thez3ro <silvethebest@yahoo.it>
2015-10-01 12:36:21 +0200

    renewed nmap

--- app/plugins/nmap.js
+++ app/plugins/nmap.js
@@ -2,22 +2,23 @@ var exec = require('child_process').exec;
 
 var execute = function (engine, cb) {
   engine.started();
-  var process = exec('nmap localhost', function (error, stdout, stderr) {
-    if (error) {
-      throw error;
-    }
-    engine.console('nmap localhost');
-    engine.console(stdout);
-    engine.console(stderr);
-    return cb(engine.ended());
+  var input = '[{"param":"address"}]';
+  engine.request_io(input);
+  engine.once('io', function (obj) {
+    obj = JSON.parse(obj);
+    exec('nmap ' + obj.address, function (error, stdout, stderr) {
+      if (error) {
+        throw error;
+      }
+      engine.console('> nmap ' + obj.address);
+      engine.console(stdout);
+      engine.console(stderr);
+      return cb(engine.ended());
+    });
   });
-}
+};
 
-var io = function () {
-
-}
 
 module.exports = {
-  'execute': execute,
-  'io': io
+  'execute': execute
 };
